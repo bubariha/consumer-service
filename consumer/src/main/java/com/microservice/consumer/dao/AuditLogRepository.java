853068be3bdeacabce17438d5044631e981a3bb4
bubariha <30747671+bubariha@users.noreply.github.com>
2021-10-06 08:41:12 +0530

    Delete consumer directory

--- consumer/src/main/java/com/microservice/consumer/dao/AuditLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.consumer.dao;
-
-import com.microservice.consumer.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Integer> {
-
-}
